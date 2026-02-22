a038998d7304f1f2c8e49bf4ba75fa7cd763cd21
Yakhrevan <114210543+Yakhrevan@users.noreply.github.com>
2026-02-22 16:19:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function createWindow() {
     const win = new BrowserWindow({
         width: 1280,
         height: 800,
-        icon: path.join(__dirname, 'icon.png'), // Optional
+        icon: path.join(__dirname, 'icon.png'),
         webPreferences: {
             nodeIntegration: false,
             contextIsolation: true
